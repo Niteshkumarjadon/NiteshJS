0f1d266d4ff533392255046d8a7c325856fe24bd
Niteshkumarjadon <113836579+Niteshkumarjadon@users.noreply.github.com>
2024-08-11 18:08:34 +0000

    Changes to the latest

---
+++ Test/variables.js
@@ -0,0 +1,9 @@
+//Variables - let
+//Constants - const
+
+let email = "nitesh@google.com"
+let userPassword = "123"
+const newUserPassword = userPassword
+console.log(email)
+console.log(userPassword)
+console.log(newUserPassword)
