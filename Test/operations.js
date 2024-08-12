e2ab802af0a1653822c95c55ece6dbdfaf856dea
Niteshkumarjadon <113836579+Niteshkumarjadon@users.noreply.github.com>
2024-08-12 05:33:41 +0000

    operations

---
+++ Test/operations.js
@@ -0,0 +1,25 @@
+// Aritemetic operators
+
+// 5+5
+// 10-5
+// 5*5
+// 10/2
+
+const numberOne =2
+const numberTwo =4
+
+const numberThree = numberOne + numberTwo
+
+console.log(numberThree)
+
+//Assignment operator
+let valueTwo = 3
+valueTwo += 10
+console.log(valueTwo)
+
+//Comparison operator
+
+let pw = 3
+let pwskills = 4
+
+console.log(pw != pwskills)
